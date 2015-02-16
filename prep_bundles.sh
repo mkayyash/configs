61c5a6d8b36de8635dfcfb21f8987c725223f0eb
Mohamad Ayyash <mkayyash@gmail.com>
2015-02-16 12:31:10 -0800

    vim: Add prep_bundles.sh script to create links for bundles.

---
+++ prep_bundles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+conf_dir=$1
+if [ ! -d "$1" ]; then
+    conf_dir=~/src/configs
+fi
+
+if [ ! -d ~/.vim ]; then
+    mkdir ~/.vim
+fi
+
+if [ ! -d ~/.vim/autoload ]; then
+    mkdir ~/.vim/autoload
+fi
+
+if [ ! -e ~/.vim/autoload/pathogen.vim ]; then
+    ln -s $conf_dir/vimplugins/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+fi
+
+if [ ! -d ~/.vim/bundle ]; then
+    ln -s $conf_dir/vimplugins ~/.vim/bundle
+fi
+
